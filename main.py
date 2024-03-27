726d86b5a03055f262c7f67758bb7eaa9d9019c4
solanin <calvintkusnadi@gmail.com>
2024-03-27 21:19:26 +1100

    Fix mechanic when having to play on a board which has already been won

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class mainWindow(tk.Tk):
 
         # main window config
         # self.configure(background='light grey')
-        # self.resizable(width=False, height=False)
+        self.resizable(width=False, height=False)
         self.title('game')
 
         # Constants
@@ -22,29 +22,30 @@ class mainWindow(tk.Tk):
         # Variables
         self.player = 'X'
         self.gamestate = np.array([[None for _ in range(9)] for _ in range(9)])   # i = row, j = column (for example, [2][7] is row 2 col 7)
-        self.boards_won = []
+        self.boards_won = {}
 
         # Adds layout
         self.add_layout()
 
+    # Adds the elements of the game (Header, Frame, Buttons)
     def add_layout(self):
         # creates label for player turn
-        self.header = tk.Label(self, text='display player turn', font=('Arial', 18))
+        self.header = tk.Label(self, text='player %s\'s turn' % self.player, font=('Arial', 18))
         self.header.grid(row=0, column=0)
 
         # creates frame for buttons
-        self.buttonframe = tk.Frame(self)
-        self.buttonframe.columnconfigure(0, weight=1)
-        self.buttonframe.columnconfigure(1, weight=1)
-        self.buttonframe.columnconfigure(2, weight=1)
-        self.buttonframe.grid(row=1, column=0)
+        self.buttonframe = tk.Frame(self, padx=10, pady=10)
+        # self.buttonframe.columnconfigure(0, weight=3)
+        # self.buttonframe.columnconfigure(1, weight=1)
+        # self.buttonframe.columnconfigure(2, weight=1)
+        self.buttonframe.grid(row=1, column=0, sticky='E')
 
         # creates game buttons
         for i in range(9):
             for j in range(9):
                 button = tk.Button(self.buttonframe, text='', font=("Arial", 24), height=1, width=3, pady=5, padx=5, command=lambda row=i, col=j: self.button_clicked(row, col))
                 self.gamestate[i][j] = button
-                button.grid(row=i, column=j, sticky='ew')
+                button.grid(row=i, column=j)
         
         self.default_button_colour = self.gamestate[0][0].cget('background')
         
@@ -52,6 +53,7 @@ class mainWindow(tk.Tk):
         self.reset = tk.Button(self, text='Reset', font=('Arial', 18), command=self.restart)
         self.reset.grid(row=2, column=0)
     
+    # When a game button is clicked...
     def button_clicked(self, row, col):
         # Updates button text AND disabled button
         self.gamestate[row][col].config(text=self.player, state='disabled')
@@ -61,29 +63,27 @@ class mainWindow(tk.Tk):
 
         # If a board has won,
         if result:
-            self.boards_won.append([board_row, board_column])
-            self.indicate_board_win(board_row, board_column)
-            self.disable_board(board_row, board_column)
+            self.boards_won[(board_row, board_column)] = self.player
+            self.indicate_board_win(board_row, board_column, self.player)
             
             messagebox.showinfo(title='title', message='player %s won in board %d %d' % (self.player, board_row, board_column))
 
-        # Updates player turn label
-        self.player = "O" if self.player == "X" else "X"
-        self.header.config(text='player %s\'s turn' % self.player)
-
-        # Indicates/sets playable board
+        # Indicates/Sets playable board
         self.disable_game_buttons()
         self.set_playable_board(row, col)
 
-        # print('clicked')
+        # Changes player turn
+        self.player = "O" if self.player == "X" else "X"
+        self.header.config(text='player %s\'s turn' % self.player)
 
+    # Checks if any board has won. Returns bool: result, int: row, int: col .
     def check_winner_board(self):
         inv_gamestate = self.gamestate.T
 
         for row in range(3):
             for col in range(3):
                 # Check if that board has won already
-                if [row,col] in self.boards_won:
+                if (row,col) in self.boards_won:
                     continue
 
                 # Check horizontals
@@ -110,6 +110,7 @@ class mainWindow(tk.Tk):
         
         return False, row, col
 
+    # Enables the buttons of the playable board.
     def set_playable_board(self, row, col):
         # Finds top corner row of playable board
         if row % 3 == 0:
@@ -127,15 +128,23 @@ class mainWindow(tk.Tk):
         if col % 3 == 2:
             board_top_corner_col = 2
         
-        self.enable_board(board_top_corner_row, board_top_corner_col)
+        # If the next playable board has already been claimed...
+        if (board_top_corner_row,board_top_corner_col) in self.boards_won:
+            self.enable_game_buttons()
 
-    def enable_board(self, top_corner_row, top_corner_col):
-        for i in range(3):
-                for j in range(3):
-                    self.gamestate[3*top_corner_row+i][3*top_corner_col+j].config(state='active', bg=self.default_button_colour)
+        # If the next playable board has not been claimed...
+        else:
+            self.enable_board(board_top_corner_row, board_top_corner_col)
+        
+        # Indicates and disables any claimed boards, by their respective X/O
+        for won_board in self.boards_won:
+            self.indicate_board_win(won_board[0],won_board[1],self.boards_won[won_board])
 
-    def indicate_board_win(self, top_corner_row, top_corner_col):
-        if self.player == "X":
+    # Places a huge X/O on a board, depending on who won that board.
+    def indicate_board_win(self, top_corner_row, top_corner_col, player):
+        self.disable_board(top_corner_row, top_corner_col)
+
+        if player == "X":
             self.gamestate[3*top_corner_row][3*top_corner_col].config(bg="black")
             self.gamestate[3*top_corner_row][3*top_corner_col+2].config(bg="black")
             self.gamestate[3*top_corner_row+1][3*top_corner_col+1].config(bg="black")
@@ -147,15 +156,28 @@ class mainWindow(tk.Tk):
             self.gamestate[3*top_corner_row+1][3*top_corner_col+2].config(bg="red")
             self.gamestate[3*top_corner_row+2][3*top_corner_col+1].config(bg="red")
 
+    # Enables the buttons of a specific board.
+    def enable_board(self, top_corner_row, top_corner_col):
+        for i in range(3):
+                for j in range(3):
+                    self.gamestate[3*top_corner_row+i][3*top_corner_col+j].config(state='active', bg=self.default_button_colour)
+
+    # Disabled the buttons of a specific board.
     def disable_board(self, top_corner_row, top_corner_col):
         for i in range(3):
                 for j in range(3):
-                    self.gamestate[3*top_corner_row+i][3*top_corner_col+j].config(state='disabled')
+                    self.gamestate[3*top_corner_row+i][3*top_corner_col+j].config(state='disabled', bg='light grey')
     
+    # Whole game functions.
     def disable_game_buttons(self):
         for i in range(9):
             for j in range(9):
                 self.gamestate[i][j].config(state='disabled', bg='light grey')
+
+    def enable_game_buttons(self):
+        for i in range(9):
+            for j in range(9):
+                self.gamestate[i][j].config(state='active', bg=self.default_button_colour)
 
     def restart(self):
         for i in range(9):
@@ -163,7 +185,7 @@ class mainWindow(tk.Tk):
                 self.gamestate[i][j].config(text='', state='active', bg=self.default_button_colour)
 
         # Reset the indictor for which boards have won
-        self.boards_won = []
+        self.boards_won = {}
 
 
 if __name__=="__main__":
