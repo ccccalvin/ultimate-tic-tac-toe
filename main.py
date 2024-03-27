3d466cce972b0907f5391c06e2799f8ff11cac2b
solanin <calvintkusnadi@gmail.com>
2024-03-27 19:20:31 +1100

    Indicates who won on an invidiual board

--- main.py
+++ main.py
@@ -15,11 +15,15 @@ class mainWindow(tk.Tk):
         # self.resizable(width=False, height=False)
         self.title('game')
 
-        # game variables
+        # Constants
+        self.turn_indicator_image = tk.PhotoImage(file='ultimate_ttt\yellow_circle.png')
+
+        # Variables
         self.player = 'X'
         self.gamestate = np.array([[None for _ in range(9)] for _ in range(9)])   # i = row, j = column (for example, [2][7] is row 2 col 7)
-        
-        # add layout
+        self.boards_won = []
+
+        # Adds layout
         self.add_layout()
 
     def add_layout(self):
@@ -28,7 +32,7 @@ class mainWindow(tk.Tk):
         self.header.grid(row=0, column=0)
 
         # creates frame for buttons
-        self.buttonframe = tk.Frame(self, background='red')
+        self.buttonframe = tk.Frame(self)
         self.buttonframe.columnconfigure(0, weight=1)
         self.buttonframe.columnconfigure(1, weight=1)
         self.buttonframe.columnconfigure(2, weight=1)
@@ -41,28 +45,42 @@ class mainWindow(tk.Tk):
                 self.gamestate[i][j] = button
                 button.grid(row=i, column=j, sticky='ew')
         
+        self.default_button_colour = self.gamestate[0][0].cget('background')
+        
         # creates reset button
         self.reset = tk.Button(self, text='Reset', font=('Arial', 18), command=self.restart)
         self.reset.grid(row=2, column=0)
     
     def button_clicked(self, row, col):
+        # Updates button text AND disabled button
         self.gamestate[row][col].config(text=self.player, state='disabled')
         
+        # Checking if a board has won everytime a button is clicked
         result, board_row, board_column = self.check_winner_board()
 
+        # If a board has won,
         if result:
+            self.boards_won.append([board_row,board_column])
+            self.indicate_board_win(board_row,board_column)
+            self.disable_board(board_row,board_column)
+            
             messagebox.showinfo(title='title', message='player %s won in board %d %d' % (self.player, board_row, board_column))
 
+        # Updates player turn label
         self.player = "O" if self.player == "X" else "X"
         self.header.config(text='player %s\'s turn' % self.player)
 
-        print('clicked')
+        # print('clicked')
 
     def check_winner_board(self):
         inv_gamestate = self.gamestate.T
 
         for row in range(3):
             for col in range(3):
+                # Check if that board has won already
+                if [row,col] in self.boards_won:
+                    continue
+
                 # Check horizontals
                 if self.gamestate[3*row][3*col].cget('text') != '' and self.gamestate[3*row][3*col].cget('text') == self.gamestate[3*row][3*col+1].cget('text') == self.gamestate[3*row][3*col+2].cget('text'):
                     return True, row, col
@@ -87,16 +105,31 @@ class mainWindow(tk.Tk):
         
         return False, None, None
 
-        
-    def disable_board1(self):
+    def indicate_board_win(self, top_corner_row, top_corner_col):
+        if self.player == "X":
+            self.gamestate[3*top_corner_row][3*top_corner_col].config(bg="black")
+            self.gamestate[3*top_corner_row][3*top_corner_col+2].config(bg="black")
+            self.gamestate[3*top_corner_row+1][3*top_corner_col+1].config(bg="black")
+            self.gamestate[3*top_corner_row+2][3*top_corner_col].config(bg="black")
+            self.gamestate[3*top_corner_row+2][3*top_corner_col+2].config(bg="black")
+        else:
+            self.gamestate[3*top_corner_row][3*top_corner_col+1].config(bg="red")
+            self.gamestate[3*top_corner_row+1][3*top_corner_col].config(bg="red")
+            self.gamestate[3*top_corner_row+1][3*top_corner_col+2].config(bg="red")
+            self.gamestate[3*top_corner_row+2][3*top_corner_col+1].config(bg="red")
+
+    def disable_board(self, top_corner_row, top_corner_col):
         for i in range(3):
                 for j in range(3):
-                    self.gamestate[i][j].config(state='disabled')
+                    self.gamestate[3*top_corner_row+i][3*top_corner_col+j].config(state='disabled')
 
     def restart(self):
         for i in range(9):
             for j in range(9):
-                self.gamestate[i][j].config(text='', state='active')
+                self.gamestate[i][j].config(text='', state='active', bg=self.default_button_colour)
+
+        # Reset the indictor for which boards have won
+        self.boards_won = []
 
 
         
